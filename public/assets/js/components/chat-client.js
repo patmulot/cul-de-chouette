ec90596ba79b8ec805abaf3f3170e3934ef975cf
patmulot <patmulot@outlook.fr>
2021-09-18 09:21:31 +0200

    ras

--- public/assets/js/components/chat-client.js
+++ public/assets/js/components/chat-client.js
@@ -11,7 +11,8 @@ let chat = {
         }
     },
     // host: "ws://" + location.host + ":8080/",
-    host: "ws://0.0.0.0:8080/",
+    // host: "ws://0.0.0.0:8080/",
+    host: "ws://127.0.0.1:8080/",
     name: "",
     socket: null,
     htmltxt: null,
