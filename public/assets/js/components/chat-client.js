05fb968aa0228f810528da0b5e7df80736df155d
patmulot <patmulot@outlook.fr>
2021-09-18 07:33:19 +0200

    ras

--- public/assets/js/components/chat-client.js
+++ public/assets/js/components/chat-client.js
@@ -10,7 +10,8 @@ let chat = {
             document.getElementById("user_name_form-go").disabled = false;
         }
     },
-    host: "ws:localhost:8080/",
+    // host: "ws:localhost:8080/",
+    host: "ws:ip-172-31-26-91/",
     // host: "ws:ec2-54-211-156-227.compute-1.amazonaws.com:8080/",
     name: "",
     socket: null,
